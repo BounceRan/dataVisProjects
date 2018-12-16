dddda184198d48355df41c762464f200d64d909e
Dan Ran <randanfx@gmail.com>
2018-12-16 00:37:43 -0500

    aa map

---
+++ AAmeetingFrontEnd/sketch.js
@@ -0,0 +1,237 @@
+var aadata1=[{"mtgaddress":"220 W HOUSTON ST New York NY ","mtspin":"null","location":"null","wheelchair":false,"meetings":[{"day":"Saturday","timeStart":"10:00 PM"},{"day":"Saturday","timeStart":"12:00 AM"},{"day":"Saturday","timeStart":"2:00 AM"},{"day":"Saturday","timeStart":"12:30 PM"},{"day":"Saturday","timeStart":"6:30 PM"},{"day":"Saturday","timeStart":"8:00 PM"}],"lat":40.7286385,"lng":-74.0045871},{"mtgaddress":"220 W HOUSTON ST New York NY ","mtspin":"Women","location":"null","wheelchair":false,"meetings":[{"day":"Saturday","timeStart":"4:45 PM"}],"lat":40.7286385,"lng":-74.0045871},{"mtgaddress":"273 BOWERY ST New York NY ","mtspin":"Topic","location":"Chinatown YMCA","wheelchair":true,"meetings":[{"day":"Saturday","timeStart":"7:30 AM"}],"lat":40.7236736,"lng":-73.9924832},{"mtgaddress":"283 W BROADWAY New York NY ","mtspin":"null","location":"Hazelden Center","wheelchair":true,"meetings":[{"day":"Saturday","timeStart":"12:00 PM"}],"lat":40.7208165,"lng":-74.0048523},{"mtgaddress":"371 6TH AVE New York NY ","mtspin":"Gay, Lesbian and Bisexual","location":"St. Joseph's Church","wheelchair":true,"meetings":[{"day":"Saturday","timeStart":"6:00 PM"}],"lat":40.6701581,"lng":-73.9819879},{"mtgaddress":"50 PERRY ST New York NY ","mtspin":"null","location":"null","wheelchair":false,"meetings":[{"day":"Saturday","timeStart":"2:30 PM"},{"day":"Saturday","timeStart":"4:00 PM"},{"day":"Saturday","timeStart":"6:00 PM"},{"day":"Saturday","timeStart":"8:30 PM"},{"day":"Saturday","timeStart":"11:00 PM"},{"day":"Saturday","timeStart":"9:30 AM"},{"day":"Saturday","timeStart":"12:00 PM"},{"day":"Saturday","timeStart":"7:30 AM"}],"lat":40.7354854,"lng":-74.0031569}];
+
+var aadata2=[{"mtgaddress":"411 E 12TH ST, New York, NY","mtspin":"Sponsorship Workshop","location":"","wheelchair":false,"meetings":[{"day":"Saturday","timeStart":"12:30 PM"}],"lat":40.729824,"lng":-73.982816},{"mtgaddress":"239 E 21ST ST, New York, NY","mtspin":"null","location":"Epiphany Roman Catholic Church","wheelchair":false,"meetings":[{"day":"Saturday","timeStart":"10:00 AM"}],"lat":40.7369141,"lng":-73.9823276},{"mtgaddress":"123 E 15TH ST, New York, NY","mtspin":"null","location":"Seafarers &amp; Intl House","wheelchair":true,"meetings":[{"day":"Saturday","timeStart":"1:00 PM"}],"lat":40.7346341,"lng":-73.9879773},{"mtgaddress":"209 E 16TH ST, New York, NY","mtspin":"null","location":"St. George's Church","wheelchair":false,"meetings":[{"day":"Saturday","timeStart":"10:30 AM"},{"day":"Saturday","timeStart":"10:30 AM"}],"lat":40.7345135,"lng":-73.9854432},{"mtgaddress":"80 SAINT MARK, New York, NY","mtspin":"null","location":"Theatre 80","wheelchair":false,"meetings":[{"day":"Saturday","timeStart":"10:00 AM"},{"day":"Saturday","timeStart":"5:30 PM"}],"lat":40.7276820090652,"lng":-73.9857361176058},{"mtgaddress":"208 W 13TH ST, New York, NY","mtspin":"null","location":"Lesbian, Gay, Bisexual, &amp; Transgender Community Center","wheelchair":true,"meetings":[{"day":"Saturday","timeStart":"8:00 PM"}],"lat":40.7381228,"lng":-74.0010304},{"mtgaddress":"50 E 7TH ST, New York, NY","mtspin":"null","location":"Middle Collegiate Church","wheelchair":false,"meetings":[{"day":"Saturday","timeStart":"7:00 PM"},{"day":"Saturday","timeStart":"7:00 PM"},{"day":"Saturday","timeStart":"8:30 PM"}],"lat":40.6519369,"lng":-73.9750389},{"mtgaddress":"411 E 12TH ST, New York, NY","mtspin":"null","location":"","wheelchair":false,"meetings":[{"day":"Saturday","timeStart":"7:30 AM"},{"day":"Saturday","timeStart":"7:30 AM"},{"day":"Saturday","timeStart":"10:00 AM"},{"day":"Saturday","timeStart":"2:00 PM"},{"day":"Saturday","timeStart":"4:00 PM"},{"day":"Saturday","timeStart":"5:30 PM"}],"lat":40.729824,"lng":-73.982816},{"mtgaddress":"208 W 13TH ST, New York, NY","mtspin":"Special Purpose Groups","location":"Lesbian, Gay, Bi-Sexual &amp; Transgender Community Center","wheelchair":false,"meetings":[{"day":"Saturday","timeStart":"9:30 PM"}],"lat":40.7381228,"lng":-74.0010304},{"mtgaddress":"80 5TH AVE, New York, NY","mtspin":"null","location":"","wheelchair":true,"meetings":[{"day":"Saturday","timeStart":"6:15 PM"}],"lat":40.7358709,"lng":-73.9939317},{"mtgaddress":"80 SAINT MARK, New York, NY","mtspin":"Women","location":"Theatre 80","wheelchair":false,"meetings":[{"day":"Saturday","timeStart":"1:00 PM"}],"lat":40.7276820090652,"lng":-73.9857361176058},{"mtgaddress":"12 W 11TH ST, New York, NY","mtspin":"null","location":"Church of the Ascension Parish House","wheelchair":false,"meetings":[{"day":"Saturday","timeStart":"7:30 AM"}],"lat":40.7341868,"lng":-73.9957178},{"mtgaddress":"208 W 13TH ST, New York, NY","mtspin":"Lesbian","location":"Lesbian, Gay, Bi-Sexual &amp; Transgender Community Center","wheelchair":true,"meetings":[{"day":"Saturday","timeStart":"6:00 PM"}],"lat":40.7381228,"lng":-74.0010304},{"mtgaddress":"19 UNION SQ W, New York, NY","mtspin":"null","location":"Realization Center","wheelchair":true,"meetings":[{"day":"Saturday","timeStart":"11:00 AM"}],"lat":40.7360864,"lng":-73.9912277},{"mtgaddress":"423 E 23RD ST, New York, NY","mtspin":"null","location":"Veterans Administration Hospital","wheelchair":true,"meetings":[{"day":"Saturday","timeStart":"9:00 PM"}],"lat":40.7368113,"lng":-73.9772015},{"mtgaddress":"28 GRAMERCY PARK S, New York, NY","mtspin":"null","location":"Christopher House","wheelchair":true,"meetings":[{"day":"Saturday","timeStart":"4:00 PM"}],"lat":40.7371499,"lng":-73.9853123},{"mtgaddress":"411 E 12TH ST, New York, NY","mtspin":"Promises","location":"","wheelchair":false,"meetings":[{"day":"Saturday","timeStart":"7:30 PM"}],"lat":40.729824,"lng":-73.982816},{"mtgaddress":"346 W 20TH ST, New York, NY","mtspin":"null","location":"St. Peters Episcopal Church- Rectory","wheelchair":false,"meetings":[{"day":"Saturday","timeStart":"8:00 AM"}],"lat":40.7441452,"lng":-74.0019191}];
+
+var aadata=[];
+
+aadata=aadata2;
+
+
+var latitudes, longitudes, location, spi, address;
+
+var circles = [];
+
+// table as the data set
+var table;
+
+// my leaflet.js map
+var mymap;
+
+
+console.log(aadata[1].meetings[0].day);
+
+
+setupMap(aadata1);
+
+// function preload() {
+//
+//
+// }
+
+const mon = document.getElementById('mon');
+mon.addEventListener('click', function(e) {
+  fetch('/mon', {method: 'GET'})
+   .then(function(response) {
+     if(response.ok) {
+       console.log('click was recorded');
+       return response.json();
+       mymap.off();
+       mymap.remove();
+     }
+     throw new Error('Request failed.');
+   })
+   .then((data)=>{
+     console.log(data);
+     setupMap(data);
+   })
+   .catch(function(error) {
+     console.log(error);
+   });
+});
+const tues = document.getElementById('tues');
+tues.addEventListener('click', function(e) {
+
+});
+const wes = document.getElementById('wes');
+wes.addEventListener('click', function(e) {
+
+});
+const thr = document.getElementById('thr');
+thr.addEventListener('click', function(e) {
+
+});
+const fri = document.getElementById('fri');
+fri.addEventListener('click', function(e) {
+
+});
+const sat = document.getElementById('sat');
+sat.addEventListener('click', function(e) {
+
+});
+const sund = document.getElementById('sund');
+sund.addEventListener('click', function(e) {
+
+});
+
+
+
+
+
+var daycheck=[ "Monday", "Tuesday","Wednesday","Thursday", "Friday", "Saturday", "Sunday"];
+
+
+function submitSearch(aadata){
+
+ // mymap.removeFrom(map);
+  //aadata=aadata1;
+  // var x = document.getElementById("dayinfo").value;
+  mymap.off();
+  mymap.remove();
+
+  // mymap.eachLayer(function (layer) {
+  //     mymap.clearLayer(layer);
+  // });
+  //mymap.leafletMap.remove();
+      //  this.leafletMap = null;
+
+
+ setupMap();
+
+};
+
+
+function setupMap(){
+
+
+
+
+      mymap = L.map('aa-map',{
+        worldCopyJump:true,
+        center: [51.505, 0.09],
+        //inertia: true
+      })//.setView([0, 0], 1);
+      .setView([40.734636,-73.994997], 13);
+
+      // var southWest = L.latLng(-90, -200),
+      // northEast = L.latLng(90, 190);
+
+      var southWest = L.latLng(-90, -Infinity),
+          northEast = L.latLng(90, Infinity);
+      var bounds = L.latLngBounds(southWest, northEast);
+
+
+     var latlng = L.latLng(89.5, 30.5)
+    var latlng = L.latLng(100.5, 200)
+      mymap.wrapLatLng(latlng);
+      mymap.setMaxBounds(bounds);
+//       mymap.on('drag', function() {
+//       mymap.panInsideBounds(bounds, { animate: false });
+// });
+
+  L.tileLayer('https://api.mapbox.com/styles/v1/randanfx/cjppzkthk0so62rml4474n2x0/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
+// L.tileLayer('https://api.mapbox.com/styles/v1/randanfx/cjpq8j2qb5dcq2snzej5x54xa/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
+      subdomains: 'abcd',
+      maxZoom: 19,
+//mapbox:/
+
+//===========================white
+     // L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+	   //    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	   // subdomains: 'abcd',
+	   // maxZoom: 19,
+
+      minZoom:1,
+      id: 'mapbox.streets',
+      accessToken: 'pk.eyJ1IjoicmFuZGFuZngiLCJhIjoiY2pwaXdqeHU1MDBtNTNxdGU5bmthMGw0YyJ9.XUsxpyY7bNWN0XUzrpAtxg',
+
+     // noWrap:true
+
+
+    }).addTo(mymap);
+
+    // call our function (defined below) that populates the maps with markers based on the table contents
+    //drawDataPoints();
+    drawMarker();
+    // controller = L.control.layers(baseMaps, overlayData, {position:"bottomright"}).addTo(map);
+
+}
+
+function drawMarker(){
+
+  var myicon = L.icon({
+      iconUrl: 'lib/leaflet/images/mymarker2.png',
+      shadowUrl: 'lib/leaflet/images/marker-shadow.png',
+
+      iconSize:     [28, 40], // size of the icon
+      shadowSize:   [41, 41], // size of the shadow
+      iconAnchor:   [14, 40], // point of the icon which will correspond to marker's location
+      shadowAnchor: [14, 41],  // the same for the shadow
+      popupAnchor:  [-3, -46] // point from which the popup should open relative to the iconAnchor
+  });
+
+
+for(var i=0; i<aadata.length; i++){
+  var aamarkers = L.marker([aadata[i].lat ,aadata[i].lng ], {icon: myicon});
+  var aaday="Meeting Info: <br>";
+  var aalocation='', spaain='';
+   if(aadata[i].location== "null"){
+     aalocation='';
+   }else{
+      aalocation= "loaction Name: "+aadata[i].location+"<br>"};
+      if(aadata[i].mtspin== "null"){
+        spaain='';
+      }else{
+         spaain= " Special Interests:"+aadata[i].mtspin +"<br>"};
+
+    for(var a=0; a<aadata[i].meetings.length; a++){
+            //return ;
+        aaday+= aadata[i].meetings[a].day +"<br> Time: " +aadata[i].meetings[a].timeStart+ "<br>";
+        aamarkers.bindPopup(aadata[i].mtgaddress+"<br>"+aalocation+" wheelchair Access: " + aadata[i].wheelchair+"<br>"+aaday+ " <br>");
+        // console.log(aaday);
+   };
+    aamarkers.addTo(mymap);
+  };
+
+};
+
+
+
+function removeAllMarks(){
+    // remove each circle from the map and empty our array of references
+    aamarkers.forEach(function(aamarkers, i){
+        mymap.removeLayer(marker);
+    })
+    circles = [];
+}
+
+
+function removeAllCircles(){
+    // remove each circle from the map and empty our array of references
+    circles.forEach(function(circle, i){
+        mymap.removeLayer(circle);
+    })
+    circles = [];
+}
+
+// get the maximum value within a column
+function getColumnMax(columnName){
+    // get the array of strings in the specified column
+    var colStrings = table.getColumn(columnName);
+
+    // convert to a list of numbers by running each element through the `float` function
+    var colValues = _.map(colStrings, float);
+
+    // find the max value by manually stepping through the list and replacing `m` each time we
+    // encounter a value larger than the biggest we've seen so far
+    var m = 0.0;
+    for(var i=0; i<colValues.length; i++){
+        if (colValues[i] > m){
+            m = colValues[i];
+        }
+    }
+    return m;
+
+    // or do it the 'easy way' by using lodash:
+    // return _.max(colValues);
+}
